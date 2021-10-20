781e7dd5fa5c68144c01f14b251e53fcec9d37d2
DoctorChe <duncan.reg@yandex.ru>
2021-10-20 18:54:17 +0300

    add the Apartment model

---
+++ backend/models/apartment.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Float, Boolean
+
+from backend.db.base_class import Base
+
+
+class Apartment(Base):
+    __tablename__ = 'apartments'
+
+    number = Column(Integer, primary_key=True, index=True)
+    floor = Column(Integer, nullable=False)
+    area = Column(Float, nullable=False)
+    rooms = Column(Integer, nullable=False)
+    balcony = Column(Boolean, nullable=False)
+    finishing = Column(Boolean, nullable=False)
+    start_price = Column(Integer, nullable=False)
