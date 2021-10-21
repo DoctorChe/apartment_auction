275595c844de867bfe88c75d77ffcfe4cdf109a4
DoctorChe <duncan.reg@yandex.ru>
2021-10-21 13:32:45 +0300

    add prefix and tags to apartments router

--- backend/api/endpoints/apartments.py
+++ backend/api/endpoints/apartments.py
@@ -7,13 +7,13 @@ from backend.api.dependencies import get_db
 router = APIRouter()
 
 
-@router.post('/apartments/', response_model=schemas.Apartment, status_code=201)
+@router.post('/', response_model=schemas.Apartment, status_code=201)
 def create_apartment(*, db: Session = Depends(get_db), payload: schemas.ApartmentIn):
     apartment = crud.create_apartment(db=db, apartment_in=payload)
     return apartment
 
 
-@router.get('/apartments/{number}/', response_model=schemas.Apartment)
+@router.get('/{number}/', response_model=schemas.Apartment)
 def read_apartment(*, db: Session = Depends(get_db), number: int = Path(..., gt=0)):
     apartment = crud.read_apartment(db=db, number=number)
     if not apartment:
@@ -21,12 +21,12 @@ def read_apartment(*, db: Session = Depends(get_db), number: int = Path(..., gt=
     return apartment
 
 
-@router.get('/apartments/', response_model=list[schemas.Apartment])
+@router.get('/', response_model=list[schemas.Apartment])
 def read_all_apartments(db: Session = Depends(get_db)):
     return crud.read_all_apartments(db)
 
 
-@router.put('/apartments/{number}/', response_model=schemas.Apartment)
+@router.put('/{number}/', response_model=schemas.Apartment)
 def update_apartment(*, db: Session = Depends(get_db),
                      number: int = Path(..., gt=0), payload: schemas.Apartment):
     apartment = crud.read_apartment(db=db, number=number)
@@ -44,7 +44,7 @@ def update_apartment(*, db: Session = Depends(get_db),
     return apartment
 
 
-@router.delete('/apartments/{number}/', response_model=schemas.Apartment)
+@router.delete('/{number}/', response_model=schemas.Apartment)
 def delete_apartment(*, db: Session = Depends(get_db), number: int = Path(..., gt=0)):
     apartment = crud.read_apartment(db=db, number=number)
     if not apartment:
--- backend/main.py
+++ backend/main.py
@@ -4,5 +4,5 @@ from backend.api.endpoints import apartments, root
 
 app = FastAPI()
 
-app.include_router(apartments.router)
+app.include_router(apartments.router, prefix='/apartments', tags=['apartments'])
 app.include_router(root.router)
